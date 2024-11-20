bbd23cf04d5463b276ad141969131445e31289f6
Lef <116380867+Stremilov@users.noreply.github.com>
2024-11-21 00:30:00 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 RUN pip install Pillow
 
-COPY . /app/.
+COPY . .
 
-ENTRYPOINT ["python", "main.py"]
+CMD ["python", "main.py"]
--- database/__init__.py
+++ database/__init__.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 class Base(DeclarativeBase):
     pass
 
+# DATABASE_URL = f"postgresql+psycopg2://postgres:postgres@db:5432/order_119"
 
-# Всё ещё изучаю, как работает async версия sqlalchemy (╥﹏╥)
 engine = create_engine("sqlite:///orderDatabase.db", pool_size=10, max_overflow=20)
 Session = sessionmaker(bind=engine)
 session = Session()
