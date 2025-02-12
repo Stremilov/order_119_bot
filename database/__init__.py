f34f46b1525cc3f72975299298fcd56e88a681d6
Lef <116380867+Stremilov@users.noreply.github.com>
2025-02-12 21:16:39 +0300

    fix

--- database/__init__.py
+++ database/__init__.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 class Base(DeclarativeBase):
     pass
 
-# DATABASE_URL = f"postgresql+psycopg2://postgres:postgres@db:5432/order_119"
+
+DATABASE_URL = f"postgresql+psycopg2://postgres:postgres@db:5432/order_119"
 
 engine = create_engine("sqlite:///orderDatabase.db", pool_size=10, max_overflow=20)
 Session = sessionmaker(bind=engine)
