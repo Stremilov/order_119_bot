e817d203a099e817ac2ff9e8337c0935d2a9dbdc
Lef <116380867+Stremilov@users.noreply.github.com>
2025-05-15 17:53:03 +0300

    fix

--- config_data/config.py
+++ config_data/config.py
@@ -1,5 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 # BOT_TOKEN = "7249176599:AAHBDwZ_xK0qXNE04tLZAQxePlNd8NxYSMY"
-BOT_TOKEN = "7386761830:AAHR8hddMBVsSIx6cvtX_7pISXEHlVaRZus"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 # BOT_TOKEN = "7531971560:AAEx67VUiUNyANcGCIMydZ8HkXKQmyYAfXI"
 CHAT_ID = '-1002154658638'
 
