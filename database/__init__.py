73451705500cfaf6100225f6ec49d7e1aa39d83e
Lef <116380867+Stremilov@users.noreply.github.com>
2025-08-31 17:24:44 +0300

    фикс

--- database/__init__.py
+++ database/__init__.py
@@ -1,13 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Base(DeclarativeBase):
     pass
 
+# Получаем параметры подключения из переменных окружения
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "order_119")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 
-DATABASE_URL = f"postgresql+psycopg2://postgres:postgres@db:5432/order_119"
+# Формируем строку подключения
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine("sqlite:///orderDatabase.db", pool_size=10, max_overflow=20)
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
 Session = sessionmaker(bind=engine)
 session = Session()
